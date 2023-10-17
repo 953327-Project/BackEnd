23798e1055871504d46bad306b479f4f602d8c8e
CVX63 <77378396+biggib01@users.noreply.github.com>
2023-10-17 12:12:24 +0000

    Update 0.5.py

--- Application/Main.py
+++ Application/Main.py
@@ -5,25 +5,34 @@ import pandas as pd
 # Create a Flask app instance
 app = Flask(__name__)
 
-csv_data = "../Material/api_lib_metrics.csv"
-metric_csv_data = "../Material/score_metrics.csv"
-df = pd.read_csv(csv_data)
+csv_metrics_data = "../Material/api_lib_metrics.csv"
+csv_scores_data = "../Material/score_metrics.csv"
+df_metrics = pd.read_csv(csv_metrics_data)
+df_scores = pd.read_csv(csv_scores_data)
 
-# Endpoint to get sorted data for a specific topic
-@app.route('/sorted/<topic>', methods=['GET'])
-def get_sorted_data(topic):
-    if topic not in df.columns:
+# Endpoint for Metrics data to get sorted data for a specific topic
+@app.route('/sortedMetrics/<topic>', methods=['GET'])
+def get_sorted_Metrics_data(topic):
+    if topic not in df_metrics.columns:
         return jsonify({"error": "Invalid topic"}), 400
 
-    sorted_data = df[['project_name', topic]].sort_values(by=topic, ascending=False).to_dict('records')
+    sorted_data = df_metrics[['project_name', topic]].sort_values(by=topic, ascending=False).to_dict('records')
     return jsonify(sorted_data)
 
+# Endpoint for Scores data to get sorted data for a specific topic
+@app.route('/sortedScores/<topic>', methods=['GET'])
+def get_sorted_Scores_data(topic):
+    if topic not in df_scores.columns:
+        return jsonify({"error": "Invalid topic"}), 400
+
+    sorted_data = df_scores[['project_name', topic]].sort_values(by=topic, ascending=False).to_dict('records')
+    return jsonify(sorted_data)
 
 
 @app.route('/get_csv_as_df', methods=['GET'])
 def get_csv_as_df():
     # Convert CSV data to a DataFram
-    df = pd.read_csv(csv_data)
+    df = pd.read_csv(csv_metrics_data)
     # Convert the DataFrame to JSON format
     json_data = df.to_json(orient='records')
     response = make_response(json_data)
@@ -35,7 +44,7 @@ def get_csv_as_df():
 @app.route('/get_score_csv', methods=['GET'])
 def get_score_csv():
     # Convert CSV data to a DataFram
-    df = pd.read_csv(metric_csv_data)
+    df = pd.read_csv(csv_scores_data)
     # Convert the DataFrame to JSON format
     json_data = df.to_json(orient='records')
     response = make_response(json_data)
